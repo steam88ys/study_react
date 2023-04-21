9b4a801e5ac1453bcc44050e04505788d2e90a18
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-04-21 09:52:46 +0900

    칸예 명언 프로그램

--- src/kanye-rest.js
+++ src/kanye-rest.js
@@ -1,38 +1,51 @@
 import React, { useState, useEffect } from "react"
 import ReactDOM from "react-dom"
 
-function KanyeQuote(props) {
-    const user = props.quote;
-    return (
-        <div>
-            quote: {props.quote}
-        </div>
-    )
-}
+function KanyeQuoteApp() {
+    // 상태
+    const [quotes, setQuotes] = useState([]);
+    const [toggle, setToggle] = useState(true);
+    const [index, setIndex] = useState(0);
 
-function KanyeApp() {
-    const [btn, setBtn] = useState(false);
-    const [quote, setQuote] = useState(null);
-    
     useEffect(() => {
-        fetch("https://api.kanye.rest/")
-        .then(res => {
-            return res.json()
-        })
-        .then(json => {
-            setQuote(json.quote)
-        })
-    }, [btn])
+        fetch("https://api.kanye.rest")
+        .then(res => res.json())
+        .then(json => setQuotes(quotes => {
+            let newQuotes = quotes.concat(json.quote);
+            setIndex(newQuotes.length-1);
+            return newQuotes;
+        }))
+    }, [toggle])
 
-    return (
-        <div>
-            {quote===null ? null : <KanyeQuote quote={quote}/>}
-            <br></br>
-            <button onClick={() =>{setBtn(btn => !btn)}}>
-            칸예 명언 보기</button> 
+    let controller;
+    if(index === 0) {
+        controller = <div>
+                <button onClick={() => {setIndex(i=>i+1)}}>▶</button>
         </div>
-       
-    )
+    } else if((index+1) === quotes.length) {
+        controller = <div>
+                <button onClick={() => {setIndex(i=>i-1)}}>◀</button>
+        </div>
+    }else {
+        controller = <div>
+                <button onClick={() => {setIndex(i=>i-1)}}>◀</button>
+                <button onClick={() => {setIndex(i=>i+1)}}>▶</button>
+        </div>
+    }
+    if(quotes.length === 1) controller = null;
+
+    return <div>
+        { quotes.length > 0 && 
+        <div>
+            <p>{index+1}/{quotes.length}</p>
+             <p>quote: {quotes[quotes.length-1]}</p>
+             {controller}
+             </div>
+        }
+        <button onClick={() => {
+            setToggle(t => !t)
+        }}>명언 불러오기</button>
+    </div>
 }
 
-ReactDOM.render(<KanyeApp />, document.getElementById("root"))
+ReactDOM.render(<KanyeQuoteApp />, document.getElementById("root"))
